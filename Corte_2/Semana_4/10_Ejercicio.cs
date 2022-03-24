65d19d3ee2b6c4d8fa47a1850aab21c98bbb02c6
Castellanos28 <castellanosdavid699@gmail.com>
2022-03-23 22:42:33 -0500

    commit

--- Corte_2/Semana_4/10_Ejercicio.cs
+++ Corte_2/Semana_4/10_Ejercicio.cs
@@ -1,28 +1,72 @@
-using System; 
+using System;
+using System.Collections;
 
 namespace Ejercicio10
 {
     class Academia
     {
-        private int [,] MatrizAcademia;
-        int CANTIDAD_FILAS=3;
-        int CANTIDAD_COLUMNAS=4;
+        private int[,] MatrizAcademia;
+        int CANTIDAD_FILAS = 3;
+        int CANTIDAD_COLUMNAS = 4;
 
         public void CargarMatriz()
         {
-            MatrizAcademia=new int[CANTIDAD_FILAS, CANTIDAD_COLUMNAS];
+            MatrizAcademia = new int[CANTIDAD_FILAS, CANTIDAD_COLUMNAS];
 
-            for(int f=0;f<CANTIDAD_FILAS;f++){
-                for(inr c=0;c<CANTIDAD_COLUMNAS;c++){
-                    Console.WriteLine("Digite la cantidad de estudiantes: ");
-                    MatrizAcademia[f,c]=int.Parse(Console.ReadLine());
+            for (int f = 0; f < CANTIDAD_FILAS; f++)
+            {
+                for (int c = 0; c < CANTIDAD_COLUMNAS; c++)
+                {
+                    Console.Write("Digite la cantidad de estudiantes: ");
+                    MatrizAcademia[f, c] = int.Parse(Console.ReadLine());
                 }
 
             }
         }
-        static void Main(string[] args){
-            Academia op=new Academia();
+        public void MostrarMatriz()
+        {
+            for (int f = 0; f < CANTIDAD_FILAS; f++)
+            {
+                if (f == 0)
+                {
+                    Console.WriteLine("\nNivel básico: ");
+
+                }
+                if (f == 1)
+                {
+                    Console.WriteLine("\nNivel medio: ");
+                }
+                if (f == 2)
+                {
+                    Console.WriteLine("\nNivel de perfeccionamiento: ");
+                }
+                for (int c = 0; c < CANTIDAD_COLUMNAS; c++)
+                {
+                    if (c == 0)
+                    {
+                        Console.Write("Inglés");
+                    }
+                    if (c == 1)
+                    {
+                        Console.Write("Francés");
+                    }
+                    if (c == 2)
+                    {
+                        Console.Write("Alemán");
+                    }
+                    if (c == 3)
+                    {
+                        Console.Write("Ruso");
+                    }
+                    Console.WriteLine(" " + MatrizAcademia[f, c]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Academia op = new Academia();
             op.CargarMatriz();
+            op.MostrarMatriz();
         }
     }
 }
